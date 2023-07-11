cfa3cdd8b5c5fadb99a14ce3ce1beb36cd911795
jroachell <jianyin.roachell@siriusxm.com>
2023-07-11 19:18:41 -0400

    cumulative plot done

--- app.R
+++ app.R
@@ -10,219 +10,168 @@
 #
 
 
-# library(shiny)
-# library(CausalImpact)
-# library(ggplot2)
-# library(readxl)
-# 
-# 
-# ui <- fluidPage(
-#   shiny::fluidRow(
-#     shinydashboard::box(
-#       shiny::actionButton(
-#         inputId = 'ab1',
-#         label = "Need Help?",
-#         icon = icon("th"),
-#         onclick = "window.open('https://github.com/Pandora-IsoMemo/CausalR/blob/main/HELP.pdf', '_blank')"
-#       )
-#     )
-#   ),
-#   titlePanel("MPI Causal Impact Dashboard v.001"),
-#   sidebarLayout(
-#     sidebarPanel(
-#       fileInput("file", "Upload File"),
-#       checkboxInput("header", "Header", TRUE),
-#       checkboxInput("treat_dates", "Treat periods as dates"),
-#       numericInput("min_pre_period", "Minimum Pre-Period Index Value", value = 1),
-#       numericInput("max_pre_period", "Maximum Pre-Period Index Value", value = 70),
-#       numericInput("min_post_period", "Minimum Post-Period Index Value", value = 71),
-#       numericInput("max_post_period", "Maximum Post-Period Index Value", value = 100),
-#       conditionalPanel(
-#         condition = "input.treat_dates",
-#         dateInput("min_pre_period_date", "Minimum Pre-Period Date", "2014-01-01"),
-#         dateInput("max_pre_period_date", "Maximum Pre-Period Date", "2014-03-11"),
-#         dateInput("min_post_period_date", "Minimum Post-Period Date", "2014-03-12"),
-#         dateInput("max_post_period_date", "Maximum Post-Period Date", "2014-04-10")
-#       ),
-#       checkboxInput("use_bsts_model", "Use BTST Model as alternative"),
-#       conditionalPanel(
-#         condition = "input.use_bsts_model",
-#         #textInput("bsts_model_text", "Custom BSTS Model code below: ",
-#         #          value = "ss <- AddLocalLevel(list(), y)  bsts.model <- bsts(y ~ x1, ss, niter = 1000)\n impact <- CausalImpact(bsts.model = bsts.model, post.period.response = post.period.response)"),
-#         textAreaInput("custom_code_text", "Custom Code:",
-#                       value = "ss <- AddLocalLevel(list(), y)
-#                       bsts.model <- bsts(y ~ x1, ss, niter = 1000)
-#                       "),
-#         ),
-#       actionButton("go", "Model"),
-#       br(),
-#       br(),
-#       tableOutput("table")
-#     ),
-#     mainPanel(
-#       plotOutput("matplot"),
-#       conditionalPanel(
-#         condition = "output.matplot",
-#         downloadButton('downloadpic1', 'Download Plot')),
-#       plotOutput("cumulative_plot"),
-#       conditionalPanel(
-#         condition = "output.cumulative_plot",
-#         downloadButton('downloadpic2', 'Download Plot')),
-#       verbatimTextOutput("results")
-#     )
-#   )
-# )
-# 
-# 
-# server <- function(input, output) {
-# 
-#   data <- reactive({
-#     req(input$file)
-#     inFile <- input$file
-#     if (endsWith(inFile$name, ".csv")) {
-#       df <- read.csv(inFile$datapath, header = input$header)
-#     } else if (endsWith(inFile$name, ".xlsx") || endsWith(inFile$name, ".xls")) {
-#       df <- read_excel(inFile$datapath)
-#     } else {
-#       return(NULL)
-#     }
-#     if (input$treat_dates){
-#       return(df)
-#     } else {
-#     df = subset(df, select = c(y,x1) )
-#     return(df)
-#     }
-#   })
-# 
-#     pre_period <- reactive({
-#       min_pre <- input$min_pre_period
-#       max_pre <- input$max_pre_period
-#       if (input$treat_dates) {
-#         min_pre <- as.Date(input$min_pre_period_date)
-#         max_pre <- as.Date(input$max_pre_period_date)
-#       }
-#       c(min_pre, max_pre)
-#     })
-# 
-#     post_period <- reactive({
-#       min_post <- input$min_post_period
-#       max_post <- input$max_post_period
-#       if (input$treat_dates) {
-#         min_post <- as.Date(input$min_post_period_date)
-#         max_post <- as.Date(input$max_post_period_date)
-#       }
-#       c(min_post, max_post)
-#     })
-# 
-# 
-#   impact_model <- eventReactive(input$go, {
-#     if (is.null(data())) return(NULL)
-# 
-#     if (input$treat_dates) {
-#       dataTime <- zoo(cbind(data()$y,data()$x1),as.Date(data()$date))
-#       return(CausalImpact(dataTime, pre_period(), post_period()))
-#     }
-#     if (input$use_bsts_model) {
-# 
-#       y <- as.ts(data()$y)
-#       x1 <- as.ts(data()$x1)
-#       post.period.response <- y[post_period()[1] : post_period()[2]]
-#       y[post_period()[1] : post_period()[2]] <- NA
-#       eval(parse(text = input$custom_code_text))
-#       #ss <- AddLocalLevel(list(), y)
-#       #bsts.model <- bsts(y ~ data()$x1, ss, niter = 2000)
-#       return(CausalImpact(bsts.model = bsts.model,post.period.response = post.period.response))
-# 
-#     }
-#     return(CausalImpact(data(), pre_period(), post_period()))
-#   })
-# 
-#   output$matplot <- renderPlot({
-#     if (is.null(data())) return(NULL)
-#     matplot(data(),type='l',main = "Time Series Intervention")
-#   })
-# 
-#   output$cumulative_plot <- renderPlot({
-#     if (is.null(impact_model())) return(NULL)
-#     plot(impact_model())
-#   })
-# 
-#   output$results <- renderPrint({
-#     if (is.null(impact_model())) return(NULL)
-#     summary(impact_model())
-#     writeLines("\nINTERPRETATION: \n")
-#     summary(impact_model(), 'report')
-#   })
-# 
-#   output$table <- renderTable(head(data()))
-#   
-#   output$downloadpic1 <- downloadHandler(
-#     filename = "matplot.png",
-#     content = function(file) {
-#       png(file)
-#       matplot(data(),type='l',main = "Time Series Intervention")
-#       dev.off()
-#     })
-#   
-#   # output$downloadpic2 <- downloadHandler(
-#   #   filename = "cumulative_plot.png",
-#   #   content = function(file) {
-#   #     png(file)
-#   #     plot(impact_model())
-#   #     dev.off()
-#   #   })
-#   
-#  # output$plot3  <- renderPlot({  data$plot })
-#   
-#   output$downloadpic2 <- downloadHandler(
-#     filename = function(){paste("cumulative_plot",'.png',sep='')},
-#     content = function(file){
-#       ggsave(file,plot=plot(impact_model()))
-#       })
-# }
-# shinyApp(ui = ui, server = server)
-
-
-
-
-
-###############
 library(shiny)
-library(shinydashboard)
-ui <- tabItem(tabName = "models2",
-              fluidPage(
-                fluidRow(
-                  infoBoxOutput("overview")
-                ),
-                fluidRow(
-                  actionButton("result1","Generate Result"),
-                  downloadButton('downloadPlot','Download Plot'),
-                  plotOutput("plot3")
-                )
-              ))
+library(CausalImpact)
+library(ggplot2)
+library(readxl)
 
-server <- function(input,output,session){
-  output$overview <- renderValueBox({
-    valueBox(
-      paste("91"),"Overview",icon=icon("hourglass"),
-      color="green"
+
+ui <- fluidPage(
+  shiny::fluidRow(
+    shinydashboard::box(
+      shiny::actionButton(
+        inputId = 'ab1',
+        label = "Need Help?",
+        icon = icon("th"),
+        onclick = "window.open('https://github.com/Pandora-IsoMemo/CausalR/blob/main/HELP.pdf', '_blank')"
+      )
     )
-  })
-  data <- reactiveValues()
-  observeEvent(input$result1,{
-    data$plot <- ggplot(data=iris, aes(x=Sepal.Length, y=Sepal.Width))+ 
-      geom_point(shape=1)})
-  
-  output$plot3  <- renderPlot({  data$plot })
-  
-  output$downloadPlot <- downloadHandler(
-    filename = function(){paste("input$plot3",'.png',sep='')},
-    content = function(file){
-      ggsave(file,plot=data$plot)
-    }
+  ),
+  titlePanel("MPI Causal Impact Dashboard v.001"),
+  sidebarLayout(
+    sidebarPanel(
+      fileInput("file", "Upload File"),
+      checkboxInput("header", "Header", TRUE),
+      checkboxInput("treat_dates", "Treat periods as dates"),
+      numericInput("min_pre_period", "Minimum Pre-Period Index Value", value = 1),
+      numericInput("max_pre_period", "Maximum Pre-Period Index Value", value = 70),
+      numericInput("min_post_period", "Minimum Post-Period Index Value", value = 71),
+      numericInput("max_post_period", "Maximum Post-Period Index Value", value = 100),
+      conditionalPanel(
+        condition = "input.treat_dates",
+        dateInput("min_pre_period_date", "Minimum Pre-Period Date", "2014-01-01"),
+        dateInput("max_pre_period_date", "Maximum Pre-Period Date", "2014-03-11"),
+        dateInput("min_post_period_date", "Minimum Post-Period Date", "2014-03-12"),
+        dateInput("max_post_period_date", "Maximum Post-Period Date", "2014-04-10")
+      ),
+      checkboxInput("use_bsts_model", "Use BTST Model as alternative"),
+      conditionalPanel(
+        condition = "input.use_bsts_model",
+        #textInput("bsts_model_text", "Custom BSTS Model code below: ",
+        #          value = "ss <- AddLocalLevel(list(), y)  bsts.model <- bsts(y ~ x1, ss, niter = 1000)\n impact <- CausalImpact(bsts.model = bsts.model, post.period.response = post.period.response)"),
+        textAreaInput("custom_code_text", "Custom Code:",
+                      value = "ss <- AddLocalLevel(list(), y)
+                      bsts.model <- bsts(y ~ x1, ss, niter = 1000)
+                      "),
+        ),
+      actionButton("go", "Model"),
+      br(),
+      br(),
+      tableOutput("table")
+    ),
+    mainPanel(
+      plotOutput("matplot"),
+      conditionalPanel(
+        condition = "output.matplot",
+        downloadButton('downloadpic1', 'Download Plot')),
+      plotOutput("cumulative_plot"),
+      conditionalPanel(
+        condition = "output.cumulative_plot",
+        downloadButton('downloadpic2', 'Download Plot')),
+      verbatimTextOutput("results")
+    )
   )
+)
+
+
+server <- function(input, output) {
+
+  data <- reactive({
+    req(input$file)
+    inFile <- input$file
+    if (endsWith(inFile$name, ".csv")) {
+      df <- read.csv(inFile$datapath, header = input$header)
+    } else if (endsWith(inFile$name, ".xlsx") || endsWith(inFile$name, ".xls")) {
+      df <- read_excel(inFile$datapath)
+    } else {
+      return(NULL)
+    }
+    if (input$treat_dates){
+      return(df)
+    } else {
+    df = subset(df, select = c(y,x1) )
+    return(df)
+    }
+  })
+
+    pre_period <- reactive({
+      min_pre <- input$min_pre_period
+      max_pre <- input$max_pre_period
+      if (input$treat_dates) {
+        min_pre <- as.Date(input$min_pre_period_date)
+        max_pre <- as.Date(input$max_pre_period_date)
+      }
+      c(min_pre, max_pre)
+    })
+
+    post_period <- reactive({
+      min_post <- input$min_post_period
+      max_post <- input$max_post_period
+      if (input$treat_dates) {
+        min_post <- as.Date(input$min_post_period_date)
+        max_post <- as.Date(input$max_post_period_date)
+      }
+      c(min_post, max_post)
+    })
+
+
+  impact_model <- eventReactive(input$go, {
+    if (is.null(data())) return(NULL)
+
+    if (input$treat_dates) {
+      dataTime <- zoo(cbind(data()$y,data()$x1),as.Date(data()$date))
+      return(CausalImpact(dataTime, pre_period(), post_period()))
+    }
+    if (input$use_bsts_model) {
+
+      y <- as.ts(data()$y)
+      x1 <- as.ts(data()$x1)
+      post.period.response <- y[post_period()[1] : post_period()[2]]
+      y[post_period()[1] : post_period()[2]] <- NA
+      eval(parse(text = input$custom_code_text))
+      #ss <- AddLocalLevel(list(), y)
+      #bsts.model <- bsts(y ~ data()$x1, ss, niter = 2000)
+      return(CausalImpact(bsts.model = bsts.model,post.period.response = post.period.response))
+
+    }
+    return(CausalImpact(data(), pre_period(), post_period()))
+  })
+
+  output$matplot <- renderPlot({
+    if (is.null(data())) return(NULL)
+    matplot(data(),type='l',main = "Time Series Intervention")
+  })
+
+  output$cumulative_plot <- renderPlot({
+    if (is.null(impact_model())) return(NULL)
+    plot(impact_model())
+  })
+
+  output$results <- renderPrint({
+    if (is.null(impact_model())) return(NULL)
+    summary(impact_model())
+    writeLines("\nINTERPRETATION: \n")
+    summary(impact_model(), 'report')
+  })
+
+  output$table <- renderTable(head(data()))
+
+  output$downloadpic1 <- downloadHandler(
+    filename = "matplot.png",
+    content = function(file) {
+      png(file)
+      matplot(data(),type='l',main = "Time Series Intervention")
+      dev.off()
+    })
+
+  output$downloadpic2 <- downloadHandler(
+    filename = function(){paste("cumulative_plot",'.png',sep='')},
+    content = function(file){
+      ggsave(file,plot= plot(impact_model()))
+      })
 }
-shinyApp(ui, server)
+
+shinyApp(ui = ui, server = server)
 
 
-    
+
